f2dcb88f245b9d4db2f0ae0f5bf1c88b219e6df3
ruthbuena <ruthbpeace@gmail.com>
2017-09-17 21:34:15 -0400

    add gitignore file and mongoos mLab info from Heroku

---
+++ server.js
@@ -0,0 +1,63 @@
+// Node Dependencies
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var logger = require('morgan');
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+
+// Initialize Express for debugging & body parsing
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Serve Static Content
+app.use(express.static(process.cwd() + '/public'));
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+// Connect to localhost if not a production environment
+if(process.env.NODE_ENV == 'production'){
+  mongoose.connect('mongodb:mongodb://heroku_ql34r6h7:et0i8gh9uvluav5fpdpckrskv3@ds141474.mlab.com:41474/heroku_ql34r6h7');
+}
+else{
+  mongoose.connect('mongodb://localhost/news-scraper');
+}
+
+var db = mongoose.connection;
+
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+Comment and Article models
+var comment = require('./models/Comment.js');
+var article = require('./models/Article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+
+var router = require('./controllers/scrape.js');
+app.use('/', router);
+
+
+// Launch App
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Running on port: ' + port);
+});
