a62e93c00aea3c7a38d9a075da2b4c22d0cfff5e
ruthbuena <ruthbpeace@gmail.com>
2017-09-17 21:49:47 -0400

    styling updates

---
+++ public/javascript/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $(".button-collapse").sideNav();
+
+  $('.add-comment-button').on('click', function(){
+
+    var articleId = $(this).data("id");
+    var baseURL = window.location.origin;
+
+    var frmName = "form-add-" + articleId;
+    var frm = $('#' + frmName);
+
+
+    //delete Comment
+    $.ajax({
+      url: baseURL + '/add/comment/' + articleId,
+      type: 'POST',
+      data: frm.serialize(),
+    })
+    .done(function() {
+      location.reload();
+    });
+    return false;
+  });
+
+
+
+  $('.delete-comment-button').on('click', function(){
+    var commentId = $(this).data("id");
+    var baseURL = window.location.origin;
+
+    //delete Comment
+    $.ajax({
+      url: baseURL + '/remove/comment/' + commentId,
+      type: 'POST',
+    })
+    .done(function() {
+      location.reload();
+    });
+
+    return false;
+  });
+});
